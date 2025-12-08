91f589d145f78b013ffccc01b0442bd1eb744e37
JJAMAWAVE <mexwell0908@gmail.com>
2025-12-09 01:10:23 +0900

    Fix: HeaderIconAnimationType.shake로 변경

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -49,7 +49,7 @@ class HomeView extends GetView<HomeController> {
                               // 친구 초대 아이콘 복원
                               HeaderIconButton(
                                 iconPath: 'assets/icons/icon_nav_notification.png',
-                                animationType: HeaderIconAnimationType.pulse,
+                                animationType: HeaderIconAnimationType.shake,
                                 onTap: () {
                                   Get.toNamed(Routes.INVITE_FRIENDS);
                                 },
